ca39eed2e2641ae37fdfa5121ce8d296a3db993a
Calipur <mycroftn@163.com>
2026-08-26 19:19:34 +0300

    (~)shimmy turn

--- Scripts/States/LeftShimmyTurnState.gd
+++ Scripts/States/LeftShimmyTurnState.gd
@@ -1,28 +1,31 @@
 class_name PlayerLeftShimmyTurnState
 extends BasePlayerState
 
-var targetPoint: Vector3
 var normal :Vector3
+var dirLeft : Vector3
+var dirForward : Vector3
 
 func Enter(player: Player) -> void:
+	dirLeft = -player.global_basis.x
+	dirForward = -player.global_basis.z
+	
 	normal = player.left_turn_climb_cast.get_collision_normal()
-	targetPoint = player.global_position
-	targetPoint -= player.global_basis.x * 1.5
-	targetPoint -= player.global_basis.z * 1
+
 
 func PreUpdate(player: Player) -> void:
-	pass
+	if player.climbable_cast.is_colliding() and player.right_climb_cast.is_colliding():
+		player.ChangeStateTo(PlayerState.HangingIdle)
 
 
 func Update(player: Player, delta: float) -> void:
-	if player.global_position.x < targetPoint.x:
-		player.velocity = Vector3.LEFT * 3
+	if player.climbable_cast.is_colliding():
+		player.velocity = dirLeft * 2.0
 		player.move_and_slide()
-	else :
-		player.velocity = -player.global_basis.z * 1
+	else:
+		player.velocity = dirForward * 2.0
 		player.move_and_slide()
-		
-	player.TurnTo(normal)
+		player.TurnTo(-normal)
+	
 	if player.floor_cast.is_colliding():
 		player.playerAnim.play("NewLib/HangLeftShimmy_short", player.BLEEND_SPEED)
 	elif not player.floor_cast.is_colliding() :
