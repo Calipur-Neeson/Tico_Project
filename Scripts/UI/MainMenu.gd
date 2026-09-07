682c1d04842eb3c01e20d80c6a29e60064964487
Calipur-Neeson <mycroftn@163.com>
2026-09-07 15:40:25 +0300

    (~)replace menu

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-@onready var play_button: Button = $CanvasLayer/Panel/PanelContainer/VBoxContainer/PlayButton
-@onready var setting_button: Button = $CanvasLayer/Panel/PanelContainer/VBoxContainer/SettingButton
-@onready var exit_button: Button = $CanvasLayer/Panel/PanelContainer/VBoxContainer/ExitButton
+@onready var new_game: Button = $CanvasLayer/MainMenu/VBoxContainer/NewGame
+@onready var control: Control = $CanvasLayer/Control
 
 
 func _ready() -> void:
@@ -10,26 +9,29 @@ func _ready() -> void:
 	if GameManager.currentInputMode == GameManager.InputMode.KEYBOARD_MOUSE:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	elif GameManager.currentInputMode == GameManager.InputMode.CONTROLLER:
-		play_button.grab_focus()
+		new_game.grab_focus()
 	
-
-func _on_play_button_pressed() -> void:
+func _on_new_game_pressed() -> void:
 	SceneLoader.LoadScene(SceneLoader.mainLevelScene)
 
-func _on_setting_button_pressed() -> void:
+func _on_load_game_pressed() -> void:
 	pass # Replace with function body.
 
-func _on_exit_button_pressed() -> void:
+func _on_options_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
+
 
 func OnInputModeChanged(mode: GameManager.InputMode) -> void:
 	if mode == GameManager.InputMode.KEYBOARD_MOUSE:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		play_button.release_focus()
-		setting_button.release_focus()
-		exit_button.release_focus()
+		var button = control.get_focus_owner()
+		button.release_focus()
+		
 
 	elif mode == GameManager.InputMode.CONTROLLER:
 		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-		play_button.grab_focus()
+		new_game.grab_focus()
 		
