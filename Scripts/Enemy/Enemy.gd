d9640d722c6d5904284fb720678bf32d320df074
Calipur-Neeson <mycroftn@163.com>
2026-09-10 14:36:07 +0300

    (+)add enemy chasing

--- Scripts/Enemy/Enemy.gd
+++ Scripts/Enemy/Enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
 @onready var enemyState: EnemeyState = $StateMachine
-@onready var playerCast: ShapeCast3D = $VisulCast3D
+@onready var playerCast: ShapeCast3D = $PlayerShape3D
 
 
 @export var moveSpeed: float = 3
@@ -25,26 +25,25 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	Move(delta)
+	if hasTarget:
+		Move(delta)
+	else:
+		velocity = Vector3.ZERO
 	move_and_slide()
 	
-	if playerCast.is_colliding():
-		
-		ChangeStateTo(enemyState.Chase)
 
 	
 func Move(delta: float) -> void:
-	if hasTarget:
-		navigation_agent_3d.target_position = targetPositon
-		var nextPathPosition: Vector3 = navigation_agent_3d.get_next_path_position()
-		var direction: Vector3 = global_position.direction_to(nextPathPosition)
-		velocity = direction * moveSpeed
-		
-		var rotSpeed = rotateSpeed
-		var targetRotation: float = direction.signed_angle_to(Vector3.MODEL_FRONT, Vector3.DOWN)
-		if abs(targetRotation - rotation.y) > deg_to_rad(60):
-			rotSpeed = rotateSpeed * 4
-		rotation.y = move_toward(rotation.y , targetRotation, delta * moveSpeed)
+	navigation_agent_3d.target_position = targetPositon
+	var nextPathPosition: Vector3 = navigation_agent_3d.get_next_path_position()
+	var direction: Vector3 = global_position.direction_to(nextPathPosition)
+	velocity = direction * moveSpeed
+	
+	var rotSpeed = rotateSpeed
+	var targetRotation: float = direction.signed_angle_to(Vector3.MODEL_FRONT, Vector3.DOWN)
+	if abs(targetRotation - rotation.y) > deg_to_rad(60):
+		rotSpeed = rotateSpeed * 4
+	rotation.y = move_toward(rotation.y , targetRotation, delta * moveSpeed)
 	
 	
 func ChangeStateTo(nextState: BaseEnemyState) -> void:
